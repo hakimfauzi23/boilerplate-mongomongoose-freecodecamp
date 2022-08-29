27a07b9eee4868c3050ac7493e1935530cb18524
Hanif Fauzi Hakim <58885246+hakimfauzi23@users.noreply.github.com>
2022-08-29 07:06:19 +0000

    Perform New Updates on a Document Using model.findOneAndUpdate()

--- myApp.js
+++ myApp.js
@@ -65,8 +65,11 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name:personName},{age:ageToSet}, {new:true},(err,updatedData) => {
+    if (err) return console.log(err);
+    done(null ,updatedData);
+  })
+  
 };
 
 const removeById = (personId, done) => {
