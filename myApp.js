bedd37feb49d5616cb87dd626a85634bb698a4b7
Hanif Fauzi Hakim <58885246+hakimfauzi23@users.noreply.github.com>
2022-08-29 01:57:19 +0000

    Use model.find() to Search Your Database

--- myApp.js
+++ myApp.js
@@ -28,8 +28,12 @@ const createManyPeople = (arrayOfPeople, done) => {
   });
 };
 
+let personName = "Robert";
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name : personName}, (err,data) => {
+    if(err) return console.log(err)
+    done(null , data);
+  });
 };
 
 const findOneByFood = (food, done) => {
